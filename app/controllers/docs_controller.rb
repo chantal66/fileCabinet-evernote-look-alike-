f17411a0155d99f40c85491869e362ba4da54fe3
Chantal Justamond <cjustamm66@me.com>
2017-11-22 12:04:52 -0700

    user can create, and show a doc

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -1,5 +1,32 @@
 class DocsController < ApplicationController
+  before_action :find_doc, only: [:show]
+
   def index
   end
 
+  def new
+    @doc = Doc.new
+  end
+
+  def create
+    @doc = Doc.new(doc_params)
+    if @doc.save
+      redirect_to @doc
+    else
+      render 'new'
+    end
+  end
+
+  def show
+  end
+
+  private
+
+  def find_doc
+    @doc = Doc.find(params[:id])
+  end
+
+  def doc_params
+    params.require(:doc).permit(:title, :content)
+  end
 end
